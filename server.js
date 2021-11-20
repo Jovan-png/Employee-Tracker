d22fedea3d60b89108d8ad365363ff431598a518
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2021-11-20 12:39:42 -0600

    added necessary folders

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const router = require('express').Router();
 const PORT = process.env.PORT || 3001;
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+
+
+
+
+
+app.listen(PORT, (req,res)=>{
+    console.log(`Sevver started on Port ${PORT}`)
+})
